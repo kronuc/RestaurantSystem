fdd0345d096766cf7cac7452a5bc4e28b9fe5df6
kronuc <denerator2@gmail.com>
2021-10-16 15:10:33 +0300

    Add project files.

---
+++ RestaurantSystem.ConsoleView/ConsoleClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RestaurantSystem.ConsoleView
+{
+    public class ConsoleClient
+    {
+        public void Start()
+        {
+            Console.WriteLine("restaurantSystem");
+        }
+    }
+}
---
+++ RestaurantSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RestaurantSystem.ConsoleView;
+
+namespace RestaurantSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleClient client = new ConsoleClient();
+            client.Start();
+        }
+    }
+}
